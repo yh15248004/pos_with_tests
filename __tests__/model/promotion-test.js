b5592d681d5fb050c9620404f7e2113f49d800ee
yh <15248004@qq.com>
2014-12-24 17:44:13 +0800

    promotion-test.js pass successful

---
+++ __tests__/model/promotion-test.js
@@ -0,0 +1,17 @@
+jest.dontMock('../../src/model/promotion');
+
+describe(',all()', function() {
+  it('should return corret array', function() {
+    var Promotion = require('../../src/model/promotion');
+
+    var result = Promotion.all();
+
+    expect(result).toEqual([
+      new Promotion('BUY_TWO_GET_ONE_FREE', [
+        'ITEM000000',
+        'ITEM000001',
+        'ITEM000005'
+        ])
+      ]);
+    });
+  });
