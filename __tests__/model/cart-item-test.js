7c7347656fd4791b500fa94d03c81dc6314376d9
yh <15248004@qq.com>
2014-12-25 19:08:01 +0800

    finish cartItem.getBarcode() unit test

--- __tests__/model/cart-item-test.js
+++ __tests__/model/cart-item-test.js
@@ -2,12 +2,14 @@ jest.dontMock('../../src/model/cart-item');
 jest.dontMock('lodash');
 
 describe('CartItem', function() {
+  var cartItem;
+  beforeEach(function() {
+    var CartItem = require('../../src/model/cart-item');
+    var cartItem = new CartItem(
+      {barcode:'ITEM000001',name:'雪碧',unit:'瓶',price:3.00},5);
+  });
   describe('#getBarcode()', function() {
     it('should return correct barcode', function() {
-
-      var CartItem = require('../../src/model/cart-item');
-      var cartItem = new CartItem(
-        {barcode:'ITEM000001',name:'雪碧',unit:'瓶',price:3.00},5);
 
       var result = cartItem.getBarcode();
 
