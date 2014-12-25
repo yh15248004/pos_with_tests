5e8316d15c0ffd20aa0ba6855320ff1574ae71b4
yh <15248004@qq.com>
2014-12-25 14:01:02 +0800

    finish addCartItem() unit test

--- __tests__/model/cart-test.js
+++ __tests__/model/cart-test.js
@@ -2,8 +2,10 @@ jest.dontMock('../../src/model/cart');
 jest.dontMock('lodash');
 
 describe('Cart', function() {
+  var cart;
   beforeEach(function() {
     var Cart = require('../../src/model/cart');
+    cart = new Cart();
   });
 
   describe('#findCartItem()', function() {
@@ -11,7 +13,7 @@ describe('Cart', function() {
 
       //var _ = require('lodash');
       var barcode = 'ITEM000001';
-      var cart = new Cart();
+      //var cart = new Cart();
       var getBarcode = jest.genMockFn();
       getBarcode.mockReturnValue('ITEM000001');
 
@@ -36,9 +38,38 @@ describe('Cart', function() {
     });
   });
 
-  // describe('#findCartItem()', function() {
-  //   it('should return correct value', function() {
-  //
-  //   });
-  // });
+  describe('#addCartItem()', function() {
+    it('should return correct value', function() {
+
+      var getBarcode = jest.genMockFn();
+      getBarcode.mockReturnValue('ITEM000001');
+
+      cart.cartItems = [{
+        item:{barcode:'ITEM000001',
+              name:'雪碧',
+              unit:'瓶',
+              price:3.00},
+        count:2,
+        getBarcode:getBarcode
+      }];
+
+      cartItem = {item:{barcode:'ITEM000001',
+                         name:'雪碧',
+                         unit:'瓶',
+                         price:3.00},
+                  count:5,
+                  getBarcode:getBarcode};
+
+      cart.addCartItem(cartItem);
+
+      expect(cart.cartItems[0].item).toEqual({
+        barcode:'ITEM000001',
+        name:'雪碧',
+        unit:'瓶',
+        price:3.00
+      });
+      expect(cart.cartItems[0].count).toEqual(7);
+    });
+  });
+
 });
