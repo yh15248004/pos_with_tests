8929e27ce10375b419ed97e067a7eca92d16d720
yh <15248004@qq.com>
2014-12-25 17:18:31 +0800

    refactor getCartItemText() & getPromotionText() unit test

--- __tests__/model/cart-test.js
+++ __tests__/model/cart-test.js
@@ -9,7 +9,7 @@ describe('Cart', function() {
   });
 
   describe('#findCartItem()', function() {
-    it('should return correct value', function() {
+    it('should return correct cartItem', function() {
 
       //var _ = require('lodash');
       var barcode = 'ITEM000001';
@@ -39,7 +39,7 @@ describe('Cart', function() {
   });
 
   describe('#addCartItem()', function() {
-    it('should return correct value', function() {
+    it('should return correct count', function() {
 
       var getBarcode = jest.genMockFn();
       getBarcode.mockReturnValue('ITEM000001');
@@ -62,41 +62,50 @@ describe('Cart', function() {
 
       cart.addCartItem(cartItem);
 
-      expect(cart.cartItems[0].item).toEqual({
-        barcode:'ITEM000001',
-        name:'雪碧',
-        unit:'瓶',
-        price:3.00
-      });
       expect(cart.cartItems[0].count).toEqual(7);
     });
+
+    it('should return correct cartItems', function() {
+      var getBarcode = jest.genMockFn();
+      getBarcode.mockReturnValue('ITEM000001');
+
+      cartItem = {item:'item',
+                  count:5,
+                  getBarcode:getBarcode};
+
+      cart.addCartItem(cartItem);
+
+      expect(cart.cartItems[0].item).toEqual('item');
+      expect(cart.cartItems[0].count).toEqual(5);
+    });
+
   });
 
   describe('#getCartItemsText()', function() {
     it('should return correct value', function() {
       var toCartItemText = jest.genMockFn();
       toCartItemText.mockReturnValue(
-        '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)');
+        'cartItemsText');
 
       cart.cartItems = [{toCartItemText:toCartItemText}];
 
       var result = cart.getCartItemsText();
 
       expect(result).toEqual(
-        '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)');
+        'cartItemsText');
     });
   });
 
   describe('#getPromotionText()', function() {
     it('should return correct value', function() {
       var toPromotionText = jest.genMockFn();
-      toPromotionText.mockReturnValue('名称：雪碧，数量：1瓶');
+      toPromotionText.mockReturnValue('promotionText');
 
       cart.cartItems = [{toPromotionText:toPromotionText}];
 
       var result = cart.getPromotionText();
 
-      expect(result).toEqual('名称：雪碧，数量：1瓶');
+      expect(result).toEqual('promotionText');
     });
 
   });
