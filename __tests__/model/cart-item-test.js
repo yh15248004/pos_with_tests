73e0d97fe41aaedd36a63e6c68c92f4f52ec7788
yh <15248004@qq.com>
2014-12-25 19:33:04 +0800

    finish CartItem.getSubtotal() unit test

--- __tests__/model/cart-item-test.js
+++ __tests__/model/cart-item-test.js
@@ -1,4 +1,5 @@
 jest.dontMock('../../src/model/cart-item');
+jest.dontMock('../../src/model/promotion');
 jest.dontMock('lodash');
 
 describe('CartItem', function() {
@@ -9,6 +10,7 @@ describe('CartItem', function() {
     cartItem = new CartItem({
       barcode:'ITEM000001',name:'雪碧',unit:'瓶',price:3.00},5);
   });
+
   describe('#getBarcode()', function() {
     it('should return correct barcode', function() {
 
@@ -17,9 +19,13 @@ describe('CartItem', function() {
       expect(result).toBe('ITEM000001');
     });
   });
-  // describe('#getSubTotal()', function() {
-  //   it('should return correct subTotal', function() {
-  //
-  //   });
-  // });
+
+  describe('#getSubTotal()', function() {
+    it('should return correct subTotal', function() {
+
+      var result = cartItem.getSubTotal();
+
+      expect(result).toEqual(12);
+    });
+  });
 });
