a41646b4587256477c019b8d7c0e9266cc675298
yh <15248004@qq.com>
2014-12-25 16:58:18 +0800

    refactor inventory toString() unit test

--- __tests__/model/inventory-test.js
+++ __tests__/model/inventory-test.js
@@ -1,6 +1,6 @@
 jest.dontMock('../../src/model/inventory');
 jest.dontMock('moment');
-//var Cart = require('../../src/model/cart.js');
+var Cart = require('../../src/model/cart.js');
 
 describe('Inventory', function() {
 
@@ -9,26 +9,17 @@ describe('Inventory', function() {
       var Inventory = require('../../src/model/inventory');
       var moment = require('moment');
 
-      var getCartItemsText = jest.genMockFunction();
-      getCartItemsText.mockReturnValue('名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
-      '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
-      '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n');
+      var cart = new Cart();
+      cart.getCartItemsText.mockReturnValue('cartItmText');
 
-      var getPromotionText = jest.genMockFunction();
-      getPromotionText.mockReturnValue('名称：雪碧，数量：1瓶\n' +
-      '名称：方便面，数量：1袋\n');
+      cart.getPromotionText.mockReturnValue('promotionText');
 
-      var getSummaryText = jest.genMockFunction();
-      getSummaryText.mockReturnValue('总计：51.00(元)\n' +
-      '节省：7.50(元)\n');
+      cart.getSummaryText.mockReturnValue('summaryText');
 
-      //var cart = new Cart();
-      //cart.getCartItemsText.mockReturnValue('');
-      
-      var cart = {
-        getCartItemsText:getCartItemsText,
-        getPromotionText:getPromotionText,
-        getSummaryText:getSummaryText
+      cart = {
+        getCartItemsText:cart.getCartItemsText,
+        getPromotionText:cart.getPromotionText,
+        getSummaryText:cart.getSummaryText
       };
 
       var inventorytext = new Inventory(cart);
@@ -38,16 +29,12 @@ describe('Inventory', function() {
       expect(result).toBe('***<没钱赚商店>购物清单***\n' +
         '打印时间：' + moment().format('YYYY年MM月DD日 HH:mm:ss') + '\n' +
         '----------------------\n' +
-        '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
-        '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
-        '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
+        'cartItmText' +
         '----------------------\n' +
         '挥泪赠送商品：\n' +
-        '名称：雪碧，数量：1瓶\n' +
-        '名称：方便面，数量：1袋\n' +
+        'promotionText' +
         '----------------------\n' +
-        '总计：51.00(元)\n' +
-        '节省：7.50(元)\n' +
+        'summaryText' +
         '**********************');
     });
   });
